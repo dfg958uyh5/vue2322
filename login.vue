6d7c71c044f2d1989bcdcc2d0587381ddb31136b
zhaoyuan123 <m15601093234@163.com>
2019-08-23 22:09:13 +0800

    哈哈哈

---
+++ login.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <el-form
+      :model="ruleForm2"
+      status-icon
+      :rules="rules2"
+      ref="ruleForm2"
+      label-width="100px"
+      class="login-container"
+    >
+      <h3>饿了么后台管理系统</h3>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input v-model.number="ruleForm2.age"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm2.checkPass !== "") {
+          this.$refs.ruleForm2.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm2: {
+        pass: "",
+        checkPass: "",
+        age: ""
+      },
+      rules2: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+           this.$router.push({ path: "/data" });
+          console.log(
+            this.ruleForm2.pass +
+              "/" +
+              this.ruleForm2.checkPass +
+              "/" +
+              this.ruleForm2.age
+          );
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    dataList() {
+      this.axios
+        .post(
+          "/api/test",
+          { name: "xiaoming", sex: "nan" },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(function(res) {
+          this.ruleForm = res.data.data;
+          console.log(res.data);
+          //控制台打印请求成功时返回的数据
+        })
+        .catch(function(err) {
+          if (err.response) {
+            console.log(err.response);
+            //控制台打印错误返回的内容
+          }
+        });
+    },
+    resetForm() {}
+  }
+};
+</script>
+
+
+
+
+
+<style scoped>
+.login-container {
+  width: 350px;
+  margin: 150px auto;
+  border: 2px solid #eaeaea;
+  padding: 35px 35px 15px 25px;
+  box-shadow: 0 0 10px blue;
+}
+.login-container h3 {
+  text-align: center;
+}
+</style>
